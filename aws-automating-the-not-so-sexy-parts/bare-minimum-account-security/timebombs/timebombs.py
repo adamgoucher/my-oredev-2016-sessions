0e475c40a4e93292412d18ea1a412c650b6a3d0f
Adam Goucher <adam@goucher.ca>
2016-08-28 16:39:41 -0400

    snapshots for ec2 and elasticache

--- aws-automating-the-not-so-sexy-parts/bare-minimum-account-security/timebombs/timebombs.py
+++ aws-automating-the-not-so-sexy-parts/bare-minimum-account-security/timebombs/timebombs.py
@@ -7,11 +7,17 @@ current_user = iam.CurrentUser()
 arn_parts = current_user.arn.split(':')
 customer_id = arn_parts[4]
 
-# list of services that support tags
+# list of services that support [billing] tags
 # http://docs.aws.amazon.com/awsaccountbilling/latest/aboutv2/allocation-how.html
+
+# in theory everything supports tags
+
+# see tag editor for types
 
 print('Amazon Elastic Block Store (Amazon EBS)')
 client = boto3.client('ec2')
+
+print('volumes')
 response = client.describe_volumes()
 for volume in response['Volumes']:
     if 'Tags' not in volume:
@@ -21,8 +27,24 @@ for volume in response['Volumes']:
         if TIMEBOMB not in tags:
             print(volume['VolumeId'])
 
+print('snapshots')
+response = client.describe_snapshots(
+    OwnerIds = [customer_id]
+)
+
+for snapshot in response['Snapshots']:
+    if 'Tags' not in snapshot:
+        print(snapshot['SnapshotId'])
+    else:
+        tags = [snapshot['Tags'][0]['Key'] for key, value in snapshot['Tags']]
+        if TIMEBOMB not in tags:
+            print(snapshot['SnapshotId'])
+
+
 print('Amazon ElastiCache (ElastiCache)')
 client = boto3.client('elasticache')
+
+print('clusters')
 response = client.describe_cache_clusters()
 for cluster in response['CacheClusters']:
     arn = 'arn:aws:elasticache:%s:%s:cluster:%s' % (cluster['PreferredAvailabilityZone'][:-1], customer_id, cluster['CacheClusterId'])
@@ -35,6 +57,7 @@ for cluster in response['CacheClusters']:
         tags = [cluster['TagList'][0]['Key'] for key, value in cluster['TagList']]
         if TIMEBOMB not in tags:
             print(cluster['CacheClusterId'])
+print('snapshots')
 
 print('TO-DO')
 print('Amazon Elastic Compute Cloud (Amazon EC2)')
